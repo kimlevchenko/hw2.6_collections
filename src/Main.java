b962ed18c603acbc8f582ee3c543c6780277314b
kimlevcenko <levchenko.kim@icloud.com>
2023-07-23 19:26:45 +0500

    выполнение ДЗ 2.6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-        System.out.println("Hello world!");
+public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+
+    public static void task1() {
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static void task2() {
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.println(num);
+                prevNum = num;
+            }
+        }
+    }
+
+    public static void task3() {
+        Set<String> uniqueWord = new HashSet<>(strings);
+        System.out.println(uniqueWord);
+    }
+
+    public static void task4() {
+        Map<String, Long> frequency = strings.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        frequency.forEach((k, v) -> System.out.println(v));
     }
 }
